fd5fde75d08d6b4e80618d0889fe95e2a5662cb8
{Jayendra <{jay.bharti2804@gmail.com}>
2025-01-17 15:03:56 +0530

    fixed timestamp to match local time

--- utils/common.utils.js
+++ utils/common.utils.js
@@ -1,18 +1,21 @@
 const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
-export const formatTimestamp = (timestamp)=>{
+export const formatTimestamp = (timestamp) => {
     let ts = new Date(timestamp);
+
+    // Convert to IST by adding 5 hours and 30 minutes
+    ts.setMinutes(ts.getMinutes() + ts.getTimezoneOffset() + 330);
 
     let date = ts.getDate();
     let month = months[ts.getMonth()];
     let year = ts.getFullYear();
     let hours = ts.getHours();
     let minutes = ts.getMinutes();
-    let ampm = hours >= 12?'pm':'am';
+    let ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
-    hours = hours?hours:12;
-    if(minutes<10){
-        minutes = `0${minutes}`
+    hours = hours ? hours : 12;
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
     }
 
     return `${date} ${month} ${year} • ${hours}:${minutes} ${ampm}`;
