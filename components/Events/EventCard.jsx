86c1670f0865bf46a61404e80c60f71ddcdef074
{Jayendra <{jay.bharti2804@gmail.com}>
2025-06-18 23:16:38 +0530

    changed event card styling

--- components/Events/EventCard.jsx
+++ components/Events/EventCard.jsx
@@ -2,6 +2,7 @@
 
 import {
   CalendarIcon,
+  ChevronRight,
   ExternalLink,
   MapPinIcon,
   TagIcon,
@@ -12,6 +13,7 @@ import Link from "next/link";
 import { formatTimestamp } from "@/utils/common.utils";
 import Image from "next/image";
 import Reveal from "../animations/Reveal";
+import { cn } from "@/lib/utils";
 
 export default function EventCard({ event, openButton = true }) {
   const compareTimestamp = (a, b) => new Date(a) < new Date(b);
@@ -48,107 +50,126 @@ export default function EventCard({ event, openButton = true }) {
   }
 
   return (
-    <Reveal>
-      <div className="max-w-md mx-auto bg-backgroundColor1 shadow-custom border border-backgroundColor1 dark:border-borderColor3 px-5 pb-5 rounded-lg mt-2">
-        {openButton && (
-          <h1 className="text-textColor1 font-bold text-2xl pt-3 text-center">
-            {event.name}
-          </h1>
-        )}
+    <Link
+      href={`/events/${event.slug.current}`}
+      onClick={(e) => {
+        if (!openButton) {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      }}
+    >
+      <Reveal className="group">
+        <div
+          className={cn(
+            "max-w-md mx-auto bg-backgroundColor1 dark:bg-backgroundColor2 shadow-custom border border-backgroundColor1 dark:border-borderColor3 p-5 rounded-3xl mt-2 hover:scale-105",
+            "shadow-custom hover:shadow-[0px_0px_100px_-20px] hover:shadow-logoColor",
+            "transition-all duration-300"
+          )}
+        >
+          {openButton && (
+            <h1 className="text-textColor1 font-bold text-2xl pt-3 text-center">
+              {event.name}
+            </h1>
+          )}
 
-        <div className="rounded-lg w-full h-max mx-auto overflow-hidden pt-3">
-          <Image
-            src={imageURL(event.image).url()}
-            alt={event.name}
-            width={500}
-            height={500}
-            className="rounded-lg mx-auto max-h-full max-w-full object-cover"
-          />
-        </div>
+          <div className="rounded-lg w-full h-max mx-auto overflow-hidden pt-3">
+            <Image
+              src={imageURL(event.image).url()}
+              alt={event.name}
+              width={500}
+              height={500}
+              className="rounded-lg mx-auto max-h-full max-w-full object-cover"
+            />
+          </div>
 
-        <div className="mt-2">
-          <div className="text-lg font-bold text-textColor2 flex flex-row justify-between p-2">
-            <span
-              className={`h-max text-sm w-min border py-0.5 px-2 rounded-lg 
+          <div className="mt-2">
+            <div className="text-lg font-bold text-textColor2 flex flex-row justify-between p-2">
+              <span
+                className={`h-max text-sm w-min border py-0.5 px-2 rounded-lg 
               ${status == "Registration Open" ? "animate-bounce" : ""}
             `}
-              style={{
-                color: `var(--${color})`,
-                borderColor: `var(--${color})`,
-              }}
-            >
-              {status}
-            </span>
-            {openButton ? (
-              <Link
-                href={`/events/${event.slug.current}`}
-                className="flex flex-row p-1 border-2 border-buttonColor text-buttonColor justify-between items-center rounded-lg hover:bg-buttonColor hover:text-[#fff] group active:ring-2 active:ring-borderColor3"
+                style={{
+                  color: `var(--${color})`,
+                  borderColor: `var(--${color})`,
+                }}
               >
-                <span>More details</span>
-                <ExternalLink className="transition-all duration-300 ml-1 group-hover:ml-4" />
+                {status}
+              </span>
+              {openButton ? (
+                // <Link
+                //   href={`/events/${event.slug.current}`}
+                //   className="flex flex-row py-1 px-2 rounded-full text-sm justify-center items-center transition-all duration-200 border-2 border-borderColor1 text-textColor2 hover:text-textColor1 group hover:border-textColor1 font-bold"
+                // >
+                //   <span>More Details</span>
+                //   <ChevronRight className="w-5 h-5 transition-all duration-200 ml-1 group-hover:ml-3" />
+                // </Link>
+                <span className="opacity-0 group-hover:opacity-100 text-textColor1 transition-all duration-300">
+                  Click to see more details
+                </span>
+              ) : (
+                <span>Event Details</span>
+              )}
+            </div>
+          </div>
+
+          <div className="grid gap-4">
+            <p className="text-muted-foreground text-textColor3">
+              {event.description}
+            </p>
+
+            <div className="flex items-center gap-2">
+              <TagIcon className="w-5 h-5 text-muted-foreground" />
+              <span className="font-medium text-sm text-textColor2">
+                {event.type}
+              </span>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <MapPinIcon className="w-5 h-5 text-muted-foreground" />
+              <span className="text-textColor2 text-sm">{event.location}</span>
+            </div>
+
+            <div className="grid gap-2">
+              <div className="flex items-center gap-2">
+                <UserPlusIcon className="w-5 h-5 text-muted-foreground" />
+                <span className="font-medium text-textColor2 text-sm">
+                  Registration Period
+                </span>
+              </div>
+              <p className="text-sm text-muted-foreground pl-7 text-textColor3">
+                {formatTimestamp(event.registrationStarts)}
+                &nbsp;&nbsp;&#x2192;&nbsp;&nbsp;
+                {formatTimestamp(event.registrationEnds)}
+              </p>
+            </div>
+
+            <div className="grid gap-2">
+              <div className="flex items-center gap-2">
+                <CalendarIcon className="w-5 h-5 text-muted-foreground" />
+                <span className="font-medium text-textColor2 text-sm">
+                  Event Duration
+                </span>
+              </div>
+              <p className="text-sm text-muted-foreground pl-7 text-textColor3">
+                {formatTimestamp(event.startTime)}
+                &nbsp;&nbsp;&#x2192;&nbsp;&nbsp;
+                {formatTimestamp(event.endTime)}
+              </p>
+            </div>
+            {status == "Registration Open" && event.registrationLink && (
+              <Link
+                href={event.registrationLink}
+                target="_blank"
+                className="border-2 border-buttonColor text-buttonColor w-full p-2 rounded-md font-bold flex flex-row justify-center items-center hover:text-[#fff] hover:bg-buttonColor"
+              >
+                <UserPlusIcon className="w-5 h-5 text-muted-foreground mr-3" />
+                Register
               </Link>
-            ) : (
-              <span>Event Details</span>
             )}
           </div>
         </div>
-
-        <div className="grid gap-4">
-          <p className="text-muted-foreground text-textColor3">
-            {event.description}
-          </p>
-
-          <div className="flex items-center gap-2">
-            <TagIcon className="w-5 h-5 text-muted-foreground" />
-            <span className="font-medium text-sm text-textColor2">
-              {event.type}
-            </span>
-          </div>
-
-          <div className="flex items-center gap-2">
-            <MapPinIcon className="w-5 h-5 text-muted-foreground" />
-            <span className="text-textColor2 text-sm">{event.location}</span>
-          </div>
-
-          <div className="grid gap-2">
-            <div className="flex items-center gap-2">
-              <UserPlusIcon className="w-5 h-5 text-muted-foreground" />
-              <span className="font-medium text-textColor2 text-sm">
-                Registration Period
-              </span>
-            </div>
-            <p className="text-sm text-muted-foreground pl-7 text-textColor3">
-              {formatTimestamp(event.registrationStarts)}
-              &nbsp;&nbsp;&#x2192;&nbsp;&nbsp;
-              {formatTimestamp(event.registrationEnds)}
-            </p>
-          </div>
-
-          <div className="grid gap-2">
-            <div className="flex items-center gap-2">
-              <CalendarIcon className="w-5 h-5 text-muted-foreground" />
-              <span className="font-medium text-textColor2 text-sm">
-                Event Duration
-              </span>
-            </div>
-            <p className="text-sm text-muted-foreground pl-7 text-textColor3">
-              {formatTimestamp(event.startTime)}
-              &nbsp;&nbsp;&#x2192;&nbsp;&nbsp;
-              {formatTimestamp(event.endTime)}
-            </p>
-          </div>
-          {status == "Registration Open" && event.registrationLink && (
-            <Link
-              href={event.registrationLink}
-              target="_blank"
-              className="border-2 border-buttonColor text-buttonColor w-full p-2 rounded-md font-bold flex flex-row justify-center items-center hover:text-[#fff] hover:bg-buttonColor"
-            >
-              <UserPlusIcon className="w-5 h-5 text-muted-foreground mr-3" />
-              Register
-            </Link>
-          )}
-        </div>
-      </div>
-    </Reveal>
+      </Reveal>
+    </Link>
   );
 }
